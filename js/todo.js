11a5007a7e956fc8bcd1ab4f2a410e4dc6471928
heeyasworld <heeyaworld@icloud.com>
2021-06-27 03:04:39 +0900

    updated delete button for todo list

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,29 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    // target된, 즉 클릭된 버튼(btn)의 parentElement는 li이고 그것을 찾아서 삭제해야 함
+    li.remove();
+}
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newToDo;
+    const btn = document.createElement("button");
+    btn.innerText = "❌";
+    btn.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(btn);
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
